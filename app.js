d9e8a10b5a8efbf370262074af5b6ec5862e9b67
Sagar712 <74832706+Sagar712@users.noreply.github.com>
2021-06-27 11:41:25 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -10,41 +10,31 @@ document.querySelector('#notification-button').onclick = async () => {
       } 
       else {
         const timestamp = new Date().getTime() + 5 * 1000; // now plus 5000ms
-        reg.showNotification(
-          'Demo Push Notification',
-          {
-            tag: timestamp, // a unique ID
-            body: 'Hello World', // content of the push notification
-            data: {
-              url: window.location.href, // pass the current url to the notification
-            }
-          }
-        );
         
+        reg.showNotification(
+            'Demo Push Notification',
+            {
+              tag: timestamp, // a unique ID
+              body: 'Hello World', // content of the push notification
+              data: {
+                url: window.location.href, // pass the current url to the notification
+              },
+              actions: [
+                {
+                  action: 'open',
+                  title: 'Open app'
+                },
+                {
+                  action: 'close',
+                  title: 'Close notification',
+                }
+              ]
+            }
+          );
       }
     });
     
 }
-
-document.querySelector('.Fire').addEventListener('click', function(){
-    'Demo Push Notification',
-    {
-      body: 'Hello World', // content of the push notification
-      data: {
-        url: window.location.href, // pass the current url to the notification
-      },
-      actions: [
-        {
-          action: 'open',
-          title: 'Open app'
-        },
-        {
-          action: 'close',
-          title: 'Close notification',
-        }
-      ]
-    }
- })
 
 navigator.serviceWorker.addEventListener('message', event => console.log(event.data));
 
