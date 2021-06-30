b14ff7ac1af29da10bb92f94c2646148e4641e70
Sagar712 <74832706+Sagar712@users.noreply.github.com>
2021-06-30 07:46:55 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -16,7 +16,22 @@ self.addEventListener("fetch", e => {
       })
   );
 
+  e.waitUntil(update(e.request))
+  .then( response => {console.log("Updated "+response)});
+
 });
+
+function update(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response.clone()).then(function () {
+        return response;
+      });
+    });
+  });
+}
+
+
 
 setInterval(function(){
   let dateIS = new Date();
